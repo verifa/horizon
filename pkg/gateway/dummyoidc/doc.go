52c8434303564106b385a830717435f014b258a7
Jacob Lärfors <1135394+jlarfors@users.noreply.github.com>
2024-02-21 20:55:30 +0200

    add doc for zitadel oidc server to say it is a copy+pasta

---
+++ pkg/gateway/dummyoidc/doc.go
@@ -0,0 +1,11 @@
+// Package dummyoidc provides a dummy OIDC server/provider for dev and testing
+// purposes.
+//
+// It is pretty much a copy+paste from the zitadel/oidc repository examples:
+//
+//	https://github.com/zitadel/oidc/tree/main/example/server
+//
+// It has a few modifications, such as allowing us to pass in a list of users.
+// Other than that, I suggest you look at the zitadel/oidc repository for more
+// information.
+package dummyoidc
